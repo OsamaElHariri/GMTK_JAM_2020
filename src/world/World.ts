42ecd9f1264dd519ae9b4b85f3df959aedcb13bd
Osama El Hariri <yamsandbread@gmail.com>
2020-07-11 16:42:54 +0300

    Add paralax rain particles

--- src/world/World.ts
+++ src/world/World.ts
@@ -21,6 +21,8 @@ export class World extends Phaser.GameObjects.Container {
     rain: Phaser.GameObjects.Particles.ParticleEmitter[] = [];
 
     rainSprite: Phaser.GameObjects.TileSprite;
+    rainSprite2: Phaser.GameObjects.TileSprite;
+    rainSprite3: Phaser.GameObjects.TileSprite;
 
     constructor(public scene: MainScene) {
         super(scene);
@@ -31,7 +33,7 @@ export class World extends Phaser.GameObjects.Container {
         player.forces = this.forces;
 
         this.windForce = {
-            sway: new Sway(50, -0.25, 1.5, 0.005),
+            sway: new Sway(150, -0.25, 1.5, 0.005),
             source: new LinearVectorSource(new Phaser.Math.Vector2(200, 200))
         };
 
@@ -45,7 +47,23 @@ export class World extends Phaser.GameObjects.Container {
         this.scene.add.sprite(200, 200, 'rock1').setScale(0.5);
 
         scene.addObject(player);
-        this.rainSprite = this.scene.add.tileSprite(0, 0, this.scene.cameras.main.width, this.scene.cameras.main.height, 'rain_particles');
+        this.rainSprite = this.scene.add.
+            tileSprite(0, 0, this.scene.cameras.main.width, this.scene.cameras.main.height, 'rain_particles')
+            .setOrigin(0)
+            .setAlpha(0.5)
+            .setDepth(200);
+        this.rainSprite2 = this.scene.add.
+            tileSprite(0, 0, this.scene.cameras.main.width, this.scene.cameras.main.height, 'rain_particles')
+            .setOrigin(0)
+            .setAlpha(0.4)
+            .setDepth(200)
+            .setTilePosition(250);
+        this.rainSprite3 = this.scene.add.
+            tileSprite(0, 0, this.scene.cameras.main.width, this.scene.cameras.main.height, 'rain_particles')
+            .setOrigin(0)
+            .setAlpha(0.3)
+            .setDepth(200)
+            .setTilePosition(500);
         // this.createWindParticles();
     }
 
@@ -109,24 +127,30 @@ export class World extends Phaser.GameObjects.Container {
         this.forces.update();
 
         const windDirection = this.windForce.source.getDirection(new Phaser.Math.Vector2(0, 0));
-        windDirection.scale(this.windForce.sway.getLength() * 2);
-        const yScale = 0.6 + NumberUtils.clamp(0, 0.4, windDirection.x / 300);
+        windDirection.scale(this.windForce.sway.getLength());
 
-        this.rainSprite.tilePositionX += 1;
-        this.rainSprite.tilePositionY += 1;
+        this.rainSprite.tilePositionX -= windDirection.x / 12;
+        this.rainSprite.tilePositionY -= windDirection.y / 12;
 
-        this.rain.forEach(rain => {
-            rain
-                .setSpeedX({
-                    min: windDirection.x, max: windDirection.x
-                })
-                .setScaleX({
-                    min: yScale, max: yScale
-                })
-                .setSpeedY({
-                    min: windDirection.y, max: windDirection.y
-                });
-        });
+        this.rainSprite2.tilePositionX -= windDirection.x / 16;
+        this.rainSprite2.tilePositionY -= windDirection.y / 16;
+
+        this.rainSprite3.tilePositionX -= windDirection.x / 20;
+        this.rainSprite3.tilePositionY -= windDirection.y / 20;
+
+        // const yScale = 0.6 + NumberUtils.clamp(0, 0.4, windDirection.x / 300);
+        // this.rain.forEach(rain => {
+        //     rain
+        //         .setSpeedX({
+        //             min: windDirection.x, max: windDirection.x
+        //         })
+        //         .setScaleX({
+        //             min: yScale, max: yScale
+        //         })
+        //         .setSpeedY({
+        //             min: windDirection.y, max: windDirection.y
+        //         });
+        // });
     }
 
     destroy() {
